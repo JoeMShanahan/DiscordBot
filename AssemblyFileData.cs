7eafb8181938f120f297150cab64373271035f80
iPeer <chrisdwignall@live.com>
2016-09-21 12:18:05 +0100

    Change date in build time to ISO-8601

--- AssemblyFileData.cs
+++ AssemblyFileData.cs
@@ -1,9 +1,9 @@
-﻿//187
+﻿//191
 // 
 // This code is automatically generated at build time. Any edits made to this file should be instead applied to its template (AssemblyFileData.tt)
 // 
 
 using System.Reflection;
 using DiscordBot.Utilities;
-[assembly: AssemblyFileVersion("1.0.5.187")]
-[assembly: AssemblyBuildTime("21/09/2016 @ 03:20:09 GMT")]
+[assembly: AssemblyFileVersion("1.0.5.191")]
+[assembly: AssemblyBuildTime("2016-09-21T11:00:26Z")]
